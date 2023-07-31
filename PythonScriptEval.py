3797b6ab3bafc32f3d9d24f7720733d1e6379aa0
Yaroslav <ykirilenko@internet.ru>
2023-08-01 01:15:22 +0400

    PythonEval script done

--- PythonScriptEval.py
+++ PythonScriptEval.py
@@ -1,1 +1,11 @@
-fedefd
+print("enter string")
+def call_eval_and_input():
+    math_string = str(input())
+    try:
+        eval(math_string)
+    except:
+        print("please input correct string")
+        math_string = call_eval_and_input()
+    return eval(str(math_string))
+print(call_eval_and_input())
+input("press enter to continue")
